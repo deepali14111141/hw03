5d6fe225de60fe9ede629d45a3f5c326825e5be5
deepali14111141 <Deepali.Worlikar@kellogg.northwestern.edu>
2022-02-21 21:13:28 +0000

    7

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,4 +5,10 @@ class PlacesController < ApplicationController
     def new
         @place = Place.new
     end
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+    
 end
