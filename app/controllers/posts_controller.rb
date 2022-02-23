707f912e236795654084af5c8e9680f7421eca1b
deepali14111141 <Deepali.Worlikar@kellogg.northwestern.edu>
2022-02-23 04:52:17 +0000

    20

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,5 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all
-end
+ 
 def new
     @post = Post.new
 end
@@ -10,7 +8,5 @@ def create
     @post.save
     redirect_to "/posts"
 end
-def show
-    @post = Post.find(params["id"])
-end
+
 end
