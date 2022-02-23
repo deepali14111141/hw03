93a82c55b31ff24dc047ce61e9e7a78759b9d39e
deepali14111141 <Deepali.Worlikar@kellogg.northwestern.edu>
2022-02-23 07:30:46 +0000

    Assignment completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,21 @@ class PostsController < ApplicationController
         @posts = Post.all
       end
     
+    def show
+      @post = Post.find(params["id"])
+    end
+
       def new
         @post = Post.new
+        @post.place_id = params["place_id"]
       end
     
       def create
-        values = params["post"]
-        @post = Post.new(values)
+        # values = params["post"]
+        @post = Post.new(params["post"])
         @post.save
         redirect_to "/posts"
+        # redirect_to "/posts/#{@post.place_id}"
       end
     
     end
